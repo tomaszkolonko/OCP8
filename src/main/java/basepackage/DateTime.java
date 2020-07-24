80242d0611d871aa70c3614515ebe19cc6ef0778
u230042 <u230042@sbb.ch>
2020-07-24 13:45:09 +0200

    created property file example

--- src/main/java/basepackage/DateTime.java
+++ src/main/java/basepackage/DateTime.java
@@ -1,49 +1,32 @@
 package basepackage;
 
-import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Period;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAdjusters;
-import java.util.Locale;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public final class DateTime {
 
     private DateTime() {}
 
     public static void main(String[] args) {
-        Locale myLocale = Locale.getDefault();
-        System.out.println("Default Locale: " + myLocale);
+        Properties properties = System.getProperties();
+        properties.list(System.out);
 
-        LocalDateTime localDateTime = LocalDateTime.of(2024, 4, 6, 13, 34, 12);
-        System.out.println("Date and Time: " + localDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
-        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of("US/Pacific"));
+        Properties ownProperties = new Properties();
+        try {
+            FileInputStream in = new FileInputStream(("src/main/resources/OCP8.properties"));
+            ownProperties.load(in);
+            ownProperties.list(System.out);
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Exception caught");
+        }
 
-        Locale locITandIT = new Locale("it", "IT");
-        Locale locITandCH = new Locale("it", "CH");
-        Locale locFRandFR = new Locale("fr", "FR");
-        Locale locXXandXX = new Locale("xx", "xx");
+        // use the properties
+        System.out.println(ownProperties.getProperty("greetingOne"));
 
-        System.out.println("locITandIT: " + zonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(locITandIT)));
-        System.out.println("locITandCH: " + zonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(locITandCH)));
-        System.out.println("locFRandFR: " + zonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(locFRandFR)));
-        System.out.println("locXXandXX: " + zonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(locXXandXX)));
-        System.out.println("locXXandXX: " + zonedDateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(myLocale)));
-
-        System.out.println("locITandIT country: " + locITandIT.getDisplayCountry());
-        System.out.println("locITandIT language: " + locITandIT.getDisplayLanguage());
-
-        Locale locBR = new Locale("pt", "BR");
-        System.out.println("Brazilian language in Brazil: " + locBR.getDisplayLanguage(locBR));
-        System.out.println("Brazilian language in US: " + locBR.getDisplayLanguage(myLocale));
+        ownProperties.setProperty("test", "test");
+        ownProperties.list(System.out);
 
 
 
