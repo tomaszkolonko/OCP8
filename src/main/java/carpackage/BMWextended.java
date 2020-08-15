a0fffea0d93d7ed6b0d65b2140ac2a115e549bb9
Tomasz Kolonko <thomas.kolonko@sbb.ch>
2020-08-15 08:30:38 +0200

    Some minor changes

--- src/main/java/carpackage/BMWextended.java
+++
@@ -1,8 +0,0 @@
-package carpackage;
-
-public class BMWextended extends BMW {
-
-    public BMWextended(int power, boolean electric, int priceTag) {
-        super(power, electric, priceTag);
-    }
-}
