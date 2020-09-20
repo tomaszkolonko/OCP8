2cbe9257062cc635fdbc4de0d5a3cf8fc0c2beee
Tomasz Kolonko <thomas.kolonko@sbb.ch>
2020-09-20 14:48:31 +0200

    Improved on DateTime Example

--- src/main/java/basepackage/DateTime.java
+++ src/main/java/basepackage/DateTime.java
@@ -1,6 +1,10 @@
 package basepackage;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,35 +22,31 @@ public final class DateTime {
         LocalDate nowDateTwo = LocalDate.of(2020, 12, 12);
         LocalDateTime nowDateTimeFour = LocalDateTime.of(2020, 12, 12, 9, 30, 23, 159);
         LocalDate nowDateThree = LocalDate.parse("2020-12-12");
+//        // Text '2020-1-12' could not be parsed at index 5
+//        LocalDate wrongFormateDate = LocalDate.parse("2020-1-12");
         LocalTime nowTimeTwo = LocalTime.parse("09:12:15");
+//        // Text '9:12:15' could not be parsed at index 0
+//        LocalTime wrongFormatTime = LocalTime.parse("9:12:15");
         LocalDateTime nowDateTimeFive = LocalDateTime.parse("2020-12-12T09:12:15");
+        LocalDateTime nowDateTimeFivee = LocalDateTime.parse("2020-12-12T09:12:15.342");
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime nowDateTimeSix = LocalDateTime.parse("2020-08-21 13:11", formatter);
 
-        System.out.println("nowDateTimeOne: " + nowDateTimeOne);
-        System.out.println("nowDateTimeTwo: " + nowDateTimeTwo);
-        System.out.println(nowDateTwo);
-        System.out.println(nowDateTimeFour);
-        System.out.println(nowDateThree);
-        System.out.println(nowTimeTwo);
-        System.out.println(nowDateTimeFive);
-        System.out.println(nowDateTimeSix);
+        System.out.println("\n========================\n");
+
+        System.out.println(nowDateTimeFive + " plus 3 days: " + nowDateTimeFive.plusDays(3));
+        System.out.println(nowDateTimeFive + " plus 3 hours: " + nowDateTimeFive.plusHours(3));
 
         System.out.println("\n========================\n");
 
-        System.out.println("plus 3 days: " + nowDateTimeFive.plusDays(3));
-        System.out.println("plus 3 hours: " + nowDateTimeFive.plusHours(3));
-
-        System.out.println("\n========================\n");
-
-        System.out.println("Day of Week: " + nowDateTimeFive.getDayOfWeek());
-        System.out.println("Day of Month: " + nowDateTimeFive.getDayOfMonth());
+        System.out.println(nowDateTimeFive + " Day of Week: " + nowDateTimeFive.getDayOfWeek());
+        System.out.println(nowDateTimeFive + " Day of Month: " + nowDateTimeFive.getDayOfMonth());
 
         System.out.println("\n========================\n");
 
         ZonedDateTime zonedDateTime = ZonedDateTime.of(nowDateTimeFour, ZoneId.of("US/Pacific"));
-        System.out.println("zonedDateTime: " + zonedDateTime);
+        System.out.println(nowDateTimeFour + " zonedDateTime: " + zonedDateTime);
 
         System.out.println("\n========================\n");
         System.out.println("Show list of all zones:");
