bc2dfe1464d7282e55ed746ccdccbb2427b4b062
Tomasz Kolonko <thomas.kolonko@sbb.ch>
2020-09-25 08:14:19 +0200

    Improved on Property File example

--- src/main/java/basepackage/DateTime.java
+++ src/main/java/basepackage/DateTime.java
@@ -1,6 +1,7 @@
 package basepackage;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -13,6 +14,25 @@ public final class DateTime {
         properties.list(System.out);
 
         Properties ownProperties = new Properties();
+
+        createNewPropertiesFile();
+        readPropertiesFile(ownProperties);
+    }
+
+    private static void createNewPropertiesFile() {
+        Properties properties = new Properties();
+        properties.setProperty("server.port", "9998");
+        properties.setProperty("debug.level", "WARN");
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream("myProperties.properties");
+            properties.store(fileOutputStream,  "Server Properties File");
+            fileOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("createNewPropertiesFile unsuccessfull " + e);
+        }
+    }
+
+    private static void readPropertiesFile(Properties ownProperties) {
         try {
             FileInputStream in = new FileInputStream(("src/main/resources/OCP8.properties"));
             ownProperties.load(in);
@@ -27,13 +47,6 @@ public final class DateTime {
 
         ownProperties.setProperty("test", "test");
         ownProperties.list(System.out);
-
-
-
-
-
-
-
     }
 
 }
