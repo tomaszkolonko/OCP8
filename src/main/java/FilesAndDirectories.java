08cd45d563afcd8c5a3c2a44196b69e7198cb0a1
u230042 <u230042@sbb.ch>
2020-10-08 19:44:39 +0200

    improved on NIO examples

--- src/main/java/FilesAndDirectories.java
+++ src/main/java/FilesAndDirectories.java
@@ -1,9 +1,9 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FilesAndDirectories {
     Path pathOne = Paths.get("/devsbb/ocp_dump/file.txt");
@@ -16,10 +16,11 @@ public class FilesAndDirectories {
         FilesAndDirectories filesAndDirectories = new FilesAndDirectories();
         filesAndDirectories.createSomeFilesAndDirectories();
         System.out.println("\n=======================\n");
-        // filesAndDirectories.copyingMovingDeletingSomeFiles();
+        filesAndDirectories.copyingMovingDeletingSomeFiles();
         System.out.println("\n=======================\n");
         filesAndDirectories.differentPathFormats();
         System.out.println("\n=======================\n");
+        filesAndDirectories.copyIntoExisting();
     }
 
     private void createSomeFilesAndDirectories() {
@@ -40,22 +41,29 @@ public class FilesAndDirectories {
             }
         }
         System.out.println(Files.exists(pathThree));
+
+//        try {
+//            Files.deleteIfExists(pathTwo);
+//            Files.deleteIfExists(pathThree);
+//        } catch (IOException e) {
+//            System.out.println("Exception caught");
+//        }
     }
 
     private void copyingMovingDeletingSomeFiles() {
         Path targetPath = Paths.get("c:\\devsbb\\ocp_dump\\file2.txt");
         Path finalPath = Paths.get("fileMoved.txt");
         try {
-            Thread.sleep(3_000);
+            Thread.sleep(2_000);
             Files.copy(pathOne, targetPath);
             System.out.println("File copied... there should be two now");
-            Thread.sleep(3_000);
+            Thread.sleep(2_000);
             Files.delete(pathOne);
             System.out.println("File deleted... there should be one now");
-            Thread.sleep(3_000);
+            Thread.sleep(2_000);
             Files.move(targetPath, finalPath);
             System.out.println("File moved... it should be now in your project");
-            Thread.sleep(4_000);
+            Thread.sleep(2_000);
             Files.delete(finalPath);
         } catch (IOException | InterruptedException e) {
             System.out.println("Exception caught:");
@@ -71,24 +79,33 @@ public class FilesAndDirectories {
 
         try {
             Files.createFile(absolutVersionOne);
-            Thread.sleep(3_000);
             Files.delete(absolutVersionOne);
 
             Files.createFile(absolutVersionTwo);
-            Thread.sleep(3_000);
             Files.delete(absolutVersionTwo);
 
             Files.createFile(absolutVersionThree);
-            Thread.sleep(3_000);
             Files.delete(absolutVersionThree);
 
             Files.createFile(absolutVersionFour);
-            Thread.sleep(3_000);
             Files.delete(absolutVersionFour);
 
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             System.out.println("Exception caught");
             e.printStackTrace();
+        }
+    }
+
+    private void copyIntoExisting() {
+        Path existingFileOne = Paths.get("c:","devsbb", "ocp_dump", "existingFileOne.txt");
+        Path target = Paths.get("c:","devsbb", "ocp_dump", "existingFileTwo.txt");
+        try {
+            Files.createFile(existingFileOne);
+            Files.createFile(target);
+//            Files.copy(existingFileOne, target);
+            Files.copy(existingFileOne, target, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.out.println("Exception caught");
         }
     }
 
