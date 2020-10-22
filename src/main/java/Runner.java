476e4dc50199b0f48236d0e57719a34146575207
u230042 <u230042@sbb.ch>
2020-10-23 00:38:22 +0200

    completed StaticNestedClasses example

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,13 @@
 public class Runner {
 
     public static void main(String[] args) {
+        StatisticalModel statisticalModel = new StatisticalModel();
+        statisticalModel.setModel();
+
+        System.out.println("\n===================\n");
+
+        StatisticalModel.Counter counter = new StatisticalModel.Counter();
+        counter.getCountInformation();
 
     }
 
---
+++ src/main/java/StatisticalModel.java
@@ -0,0 +1,12 @@
+public class StatisticalModel {
+
+    static class Counter {
+        void getCountInformation() {
+            System.out.println("Count Information about the Statistical Model");
+        }
+    }
+
+    void setModel() {
+        System.out.println("Setting up the model");
+    }
+}
