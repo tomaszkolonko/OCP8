79de284a5bf0634d3cc313ee294ccdd8b3f08777
u230042 <u230042@sbb.ch>
2020-10-23 14:08:54 +0200

    improved on example

--- src/main/java/Countable.java
+++ src/main/java/Countable.java
@@ -1,3 +1,5 @@
 public interface Countable {
+    // I thought there must be the abstract keyword for it to be a
+    // functional interface...
     void getCurrentCount();
 }
--- src/main/java/MyMoleskin.java
+++ src/main/java/MyMoleskin.java
@@ -6,17 +6,18 @@ public class MyMoleskin {
         MyMoleskin myMoleskin = new MyMoleskin();
 
         TodoList todoList = new TodoList();
+
+        SomeOtherClass someOtherClass = new SomeOtherClass();
+        todoList.addItem(someOtherClass);
+
+        System.out.println("\n================\n");
+
         todoList.addItem(new Countable() {
             @Override
             public void getCurrentCount() {
                 System.out.println("42");;
             }
         });
-
-        System.out.println("\n================\n");
-
-        SomeOtherClass someOtherClass = new SomeOtherClass();
-        todoList.addItem(someOtherClass);
 
         System.out.println("\n================\n");
 
