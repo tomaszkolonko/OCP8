5acdd4049102c7f65c4bc74128644d8ccdbee39c
u230042 <u230042@sbb.ch>
2020-11-13 08:50:38 +0100

    completed example on WaitAndNotify

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,8 +2,23 @@
 
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadB thread = new ThreadB();
+        thread.start();
 
+
+//        Thread.sleep(1000);
+
+        System.out.println("Total of computation is: " + thread.total);
+
+        synchronized (thread) {
+            try {
+                System.out.println("Waiting for the thread to complete...");
+                thread.wait();
+            } catch (InterruptedException e) {
+                System.out.println("Total is: " + thread.total);
+            }
+        }
     }
 
 }
