165167a62953921350adc3d078936224d5350eb1
u230042 <u230042@sbb.ch>
2020-11-27 00:02:18 +0100

    completed example on ResultSets

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -21,9 +22,7 @@ public class Runner {
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected successfully");
 
-            createANewBookTable(connection);
-            populateTheBookTable(connection);
-            System.out.println("\n=========================================\n");
+            readBookTableMetaData(connection);
             readBookTable(connection);
 
         } catch (SQLException e) {
@@ -36,41 +35,6 @@ public class Runner {
                     System.out.println("Something went wrong while closing the connction");
                 }
             }
-        }
-    }
-
-    public static void createANewBookTable(Connection connection) {
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            String createTable = "CREATE TABLE books (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, isbn VARCHAR (20), title VARCHAR (50),  price INT)";
-            statement.executeUpdate(createTable);
-            System.out.println("Successfully created a new table named 'books'");
-        } catch (SQLException e) {
-            System.out.println("Error during creating Table books");
-        } finally {
-            closeStatement(statement);
-        }
-    }
-
-    public static void populateTheBookTable(Connection connection) {
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            String insertBooks = "INSERT INTO books(isbn, title, price) VALUES"
-                    + "('123-123', 'The nice Sunset', 23),"
-                    + "('443-295', 'Coding Styles by Martin', 99),"
-                    + "('635-152', 'How to find a Hobby', 19),"
-                    + "('837-368', 'Driving a car mad easy', 89),"
-                    + "('443-111', 'History of SBB', 15),"
-                    + "('599-128', 'How to invest in stocks', 23),"
-                    + "('443-223', 'Fermenting Sauerkraut made easy', 31)";
-            statement.execute(insertBooks);
-            System.out.println("Successfully populated the 'books' table: ");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            closeStatement(statement);
         }
     }
 
@@ -92,6 +56,30 @@ public class Runner {
         }
     }
 
+    public static void readBookTableMetaData(Connection connection) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String retrieveBooks = "SELECT * FROM books";
+            ResultSet resultSet = statement.executeQuery(retrieveBooks);
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            resultSet.next();
+
+            int columnCount = resultSetMetaData.getColumnCount();
+
+            for(int i = 1; i <= columnCount; i++) {
+                System.out.println("Table name: " + resultSetMetaData.getTableName(i));
+                System.out.println("Column name: " + resultSetMetaData.getColumnName(i));
+                System.out.println("Column size: " + resultSetMetaData.getColumnDisplaySize(i));
+                System.out.println("Column class name: " + resultSetMetaData.getColumnClassName(i));
+                System.out.println("\n=============================\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private static void closeStatement(Statement statement) {
         try {
