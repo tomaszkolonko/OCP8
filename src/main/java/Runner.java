6200b4ab75b7b0aaecb6bdb096106ef0e357852b
u230042 <u230042@sbb.ch>
2020-11-27 08:16:34 +0100

    completed example on DB capabilities

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,8 @@
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Runner {
 
@@ -20,10 +19,32 @@ public class Runner {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected successfully");
+            System.out.println("Connected successfully\n");
 
-            readBookTableMetaData(connection);
-            readBookTable(connection);
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            if (databaseMetaData.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY)) {
+                System.out.print("Supports TYPE_FORWARD_ONLY");
+                if (databaseMetaData.supportsResultSetConcurrency(ResultSet.TYPE_FORWARD_ONLY,
+                        ResultSet.CONCUR_UPDATABLE)) {
+                    System.out.println(" and supports CONCUR_UPDATABLE");
+                }
+            }
+
+            if (databaseMetaData.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE)) {
+                System.out.print("Supports TYPE_SCROLL_INSENSITIVE");
+                if (databaseMetaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_UPDATABLE)) {
+                    System.out.println(" and supports CONCUR_UPDATABLE");
+                }
+            }
+
+            if (databaseMetaData.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE)) {
+                System.out.print("Supports TYPE_SCROLL_SENSITIVE");
+                if (databaseMetaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_SENSITIVE,
+                        ResultSet.CONCUR_UPDATABLE)) {
+                    System.out.println(" and supports CONCUR_UPDATABLE");
+                }
+            }
 
         } catch (SQLException e) {
             System.out.println("SQL Connection failed");
@@ -35,57 +56,6 @@ public class Runner {
                     System.out.println("Something went wrong while closing the connction");
                 }
             }
-        }
-    }
-
-    public static void readBookTable(Connection connection) {
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            String retrieveBooks = "SELECT * FROM books";
-            ResultSet resultSet = statement.executeQuery(retrieveBooks);
-            while (resultSet.next()) {
-                System.out.print("ID: " + resultSet.getString("id") + "   ");
-                System.out.print("ISBN: " + resultSet.getString("isbn") + "   ");
-                System.out.println("TITLE: " + resultSet.getString("title"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            closeStatement(statement);
-        }
-    }
-
-    public static void readBookTableMetaData(Connection connection) {
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            String retrieveBooks = "SELECT * FROM books";
-            ResultSet resultSet = statement.executeQuery(retrieveBooks);
-            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-
-            resultSet.next();
-
-            int columnCount = resultSetMetaData.getColumnCount();
-
-            for(int i = 1; i <= columnCount; i++) {
-                System.out.println("Table name: " + resultSetMetaData.getTableName(i));
-                System.out.println("Column name: " + resultSetMetaData.getColumnName(i));
-                System.out.println("Column size: " + resultSetMetaData.getColumnDisplaySize(i));
-                System.out.println("Column class name: " + resultSetMetaData.getColumnClassName(i));
-                System.out.println("\n=============================\n");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void closeStatement(Statement statement) {
-        try {
-            statement.close();
-        } catch (SQLException e) {
-            System.out.println("Error during closing statemetn");
         }
     }
 
